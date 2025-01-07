3cdd9cdfa9a94094259af57b1bda3205b7fde6e0
DEV-COM <kwp@tlen.pl>
2025-01-07 16:51:55 +0100

    Remove unused flag management and total price calculation files

--- src/getTotal.js
+++
@@ -1,8 +0,0 @@
-var unitPrice;
-unitPrice = 345;
-function getTotal(unitPrice, quan, discount) {
-    var priceWithoutDisc = unitPrice * quan;
-    var discAmount = priceWithoutDisc * discount;
-    return priceWithoutDisc - discAmount;
-}
-var total = getTotal(500, 5, 50);
--- src/totalPrice.js
+++
@@ -1,5 +0,0 @@
-function calculate(product, quantity, discount) {
-    var priceWithoutDisc = product.unitPrice * quantity;
-    var discAmount = priceWithoutDisc * discount;
-    return priceWithoutDisc - discAmount;
-}
